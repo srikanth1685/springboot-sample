c51e7e70af3b064174140d62b81dc85bffa5c919
Osomudeya Zudonu <talk2zudonuosomudeya@gmail.com>
2023-06-22 00:28:13 +0100

    Update StartApplication.java

--- src/main/java/com/devops/coach/StartApplication.java
+++ src/main/java/com/devops/coach/StartApplication.java
@@ -12,7 +12,7 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "Howdy Folks..Welcome to Devops Coaching by Coach AK!");
+        model.addAttribute("title", "Howdy Folks..Welcome to Devops Coaching by Osomudeya!");
         model.addAttribute("msg", "Hello All..We are testing springboot deployment with Helm and Jenkins Pipeline!!!!");
         return "index";
     }
